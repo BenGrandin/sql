e7ba10dfb177ea30fe94c1d1bc76a6d0a1f2f61e
Flow2dot0 <44232408+Flow2dot0@users.noreply.github.com>
2018-10-25 20:13:35 +0200

    ajout id utilisateur story 5

--- sommaire.sql
+++ sommaire.sql
@@ -181,7 +181,7 @@ WHERE id = 26;
 /*story 5*/
 
 INSERT INTO service (id_utilisateur, nom_service, description_service, adresse_service, code_postale_service, ville_service, pays_service, date_heure_service, info_complementaire)
-VALUES ('bergerie', 'savoir gérer la brebis', 'boulevard du berger perdu', '64340', 'shepherdland', 'france', '1991-08-21 21:11:14', 'jaime les berger');
+VALUES (8, 'bergerie', 'savoir gérer la brebis', 'boulevard du berger perdu', '64340', 'shepherdland', 'france', '1991-08-21 21:11:14', 'jaime les berger');
 
 /*story 6*/
 
